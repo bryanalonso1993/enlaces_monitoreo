b79ee0cc9d9080095cb4df08a149325625336483
bryan alonso <bryalmeyda.7@gmail.com>
2021-10-04 19:18:31 -0500

    ultima modificacion

--- controllers/processData.js
+++ controllers/processData.js
@@ -4,59 +4,83 @@ const logger = require('../config/logger');
 
 require('../config/config');
 /**
- * Parametros del API del PM
- */
-const axios = require('axios').default;
-const encodedBase64 = Buffer.from(`${process.env.APIUSRPM}:${process.env.APIPWDPM}`).toString('base64');
-const authorization = `Basic ${encodedBase64}`;
-const opts = { withCredentials: true, headers: {"Accept":"application/json", "Content-Type":"application/json", "Authorization": authorization} };
-
-/**
  * DB
  */
 const connection = require('../db/connection');
 const intervalSecondTimeQuery = 600;
 const queryString = (deviceName, interfaces) => {
     let query = `select A.device_name,A.interface,A.timestamp_polling,A.BitsPerSecondIn from (select device_name,interface,
-    timestamp_polling,BitsPerSecondIn from ${deviceName} where timestamp_polling between UNIX_TIMESTAMP(CURRENT_TIMESTAMP() - interval ${intervalSecondTimeQuery} second)
+    timestamp_polling,BitsPerSecondIn from enlaces.${deviceName} where timestamp_polling between UNIX_TIMESTAMP(CURRENT_TIMESTAMP() - interval ${intervalSecondTimeQuery} second)
     AND UNIX_TIMESTAMP(CURRENT_TIMESTAMP())) as A where A.interface in (${interfaces})`;
     return query;
 }
 
 function processInterfaces (storage) {
-    let interfaceString = "";
+    let interfaceString = '';
     for (let index=0; index<storage.length - 1; index++) {
-        interfaceString += `"${storage[index]}",`;
+        interfaceString += `'${storage[index]}',`;
     }
-    interfaceString += `"${storage[storage.length - 1]}"`;
+    interfaceString += `'${storage[storage.length - 1]}'`;
     return interfaceString;
 }
 
 function processAllQueries(db) {
-    let fullQuery = "";
+    let fullQuery = '';
     for (let index=0;index<db.length;index++){
         let { deviceName, interfaces } = db[index];
-        fullQuery += queryString(deviceName, processInterfaces(interfaces)).concat(" UNION ALL ");
+        fullQuery += queryString(deviceName, processInterfaces(interfaces)).concat(' UNION ALL ');
     }
-    fullQuery += queryString(db[db.length - 1].deviceName, db[db.length - 1].interfaces);
+    fullQuery += queryString(db[db.length - 1].deviceName, processInterfaces(db[db.length - 1].interfaces));
     return fullQuery;
 }
 
 /**
  * API
  */
+const axios = require('axios').default;
+const encodedBase64 = Buffer.from(`${process.env.APIUSRPM}:${process.env.APIPWDPM}`).toString('base64');
+const authorization = `Basic ${encodedBase64}`;
+const opts = { withCredentials: true, headers: {"Accept":"application/json", "Content-Type":"application/json", "Authorization": authorization} };
+const intervalTimeApiPM = "900s"
+const queryEndpointPM = (deviceName, interface) => {
+    return `http://${process.env.APIIPADDR}:${APIPORTPM}/odata/api/interfaces?$format=json&$top=500&$skip=0&top=100&period=${intervalTimeApiPM}&$expand=device,portmfs&$select=device/Name,device/PrimaryIPAddress,DisplayName,IPAddresses,portmfs/Timestamp,portmfs/im_BitsPerSecondIn,portmfs/im_BitsPerSecondOut&$filter=((device/Name eq '${deviceName}') and (DisplayName eq '${interface}'))`;
+}
 
 
-// metodo de consulta : http://127.0.0.1:7070/:method/?device=&interface=
-const getDataServer = (req=request, res=response) => {
+// metodo de consulta : http://127.0.0.1:7070/:method/
+const getDataServer = async (req=request, res=response) => {
     const { method } = req.params;
     const data = req.body;
     switch (method.toLowerCase()) {
-        case 'bd':
-            connection.query();
+        case 'db':
+            const totalQuery = processAllQueries(data);
+            await connection.query(totalQuery)
+                .then(response => {
+                    logger.log({level:'info', message: `Success process query ${totalQuery}`});
+                    res.status(200).json({ data: response});
+                })
+                .catch(e => {
+                    captureErrors("dbquery", `Error query db: ${ e?.original?.text }`);
+                    res.status(500).send({ error: {source: e?.name, errorText: e?.original?.text , errorCode: e?.original?.code }});
+                })
             break;
         case 'api':
-            res.send('OK');
+            await axios.get(queryEndpointPM(data[0].deviceName, data[0].interface), opts)
+                .then(response => {
+                    const data = response.d.results[0];
+                    let storageData = new Array();
+                    data.portmfs.results.forEach(element => {
+                        storageData = [...storageData, {deviceName: data.device.Name, timestamp: element.Timestamp, BitsPerSecondIn: element.im_BitsPerSecondIn}];
+                    });
+                    res.status(200).json({
+                        data: storageData
+                    })
+                })
+                .catch(e => {
+                    res.status(500).json({
+                        error: e
+                    })
+                })
             break;
         default:
             break;
--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,7 @@
 const captureErrors = require('./captureErrors');
-const endpoints = require('./endpoints');
 const filesystem = require('./filesystem');
 
 module.exports = {
     captureErrors,
-    endpoints,
     filesystem
 }
