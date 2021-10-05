e782039d52a64dc329de1eca1f1c22ecc7276b8e
bryan alonso <bryalmeyda.7@gmail.com>
2021-10-04 19:19:12 -0500

    cambios prod en archivo config

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@
 process.env.PORT =  process.env.PORT || 7070;
 
 // endpoint apm - server
-//process.env.APMSERVER = "http://10.95.164.83:8200";
-process.env.APMSERVER = "http://192.168.0.97:8200";
+process.env.APMSERVER = "http://10.95.164.83:8200";
+//process.env.APMSERVER = "http://192.168.0.97:8200";
 
 // api credentials
 process.env.USERAPI = "nevermore";
@@ -13,8 +13,8 @@ process.env.PASSAPI = "$@T3st123@$";
 process.env.SEED = "$#S3credSeedClaro2021#$";
 
 // parameters database mariadb
-//process.env.HOSTDB = "172.19.216.110";
-process.env.HOSTDB = "127.0.0.1";
+process.env.HOSTDB = "172.19.216.110";
+//process.env.HOSTDB = "127.0.0.1";
 process.env.ENGINEDB = "mariadb";
 process.env.PORTDB = 3306;
 process.env.DB = "semaforos";
